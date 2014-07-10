0c48a076d8d63b223f480ca6fdfae2b06618782e
Bob Owen <bob@bobowen.org>
2014-07-10 16:15:54 -0700

    Internally renamed app to gmajor

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('gmajor.controllers', [])
 
 .controller('GridCtrl', function($scope) {
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('gmajor.services', [])
 
 /**
  * A simple example service that returns some data.

1553274e9a5e7c4f6a3090b0754ae08c89b14c07
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-11 15:24:54 -0700

    Switch to express

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require('http')
+var express = require('express');
+
+var server = express();
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+server.get('/', function(req, res){
+  res.send('hello world');
+});
+server.listen(port);
+
+

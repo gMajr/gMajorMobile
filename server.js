a9761018508a18b28c2d193833b15ade63614c16
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-11 15:32:20 -0700

    Serve static files

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 
 var server = express();
+server.use(express.static(__dirname + '/www'));
 var port = process.env.PORT || 1337;
-server.get('/', function(req, res){
-  res.send('hello world');
-});
 server.listen(port);
-
-

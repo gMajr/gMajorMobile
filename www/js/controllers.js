264eb8127e4623424007c09daa4ef31dae0ab8be
Bob Owen <bob@bobowen.org>
2014-07-11 12:14:46 -0700

    Grid displays based on controller array of columns composed of targets.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,6 +13,11 @@ angular.module('gmajor.controllers', [])
 .controller('GridController', function ($scope) {
   $scope.navTitle = 'Grid Yo!';
 
+  $scope.columns = [[{name: 1},{name: 2} ,{name: 3} ,{name: 4}],
+                    [{name: 5},{name: 6} ,{name: 7} ,{name: 8}],
+                    [{name: 9},{name: 10} ,{name: 11} ,{name: 12}],
+                    [{name: 13},{name: 14} ,{name: 15} ,{name: 16}]];
+
   $scope.leftButtons = [{
     type: 'button-icon icon ion-navicon',
     tap: function(e) {

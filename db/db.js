ee99970853eccf3a81be2279759e8e292b024aa0
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-14 16:53:15 -0700

    Mongo server starts and saves a user on initialization

--- db/db.js
+++ db/db.js
@@ -1,10 +1,25 @@
+var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://MongoLab-r:MBC1Zu.wH4o6g02MiEDSxOml1YfGewuVHMG1Ofe9Exc-@ds050077.mongolab.com:50077/MongoLab-r';
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 module.exports= {
   init: function(){
-    MongoClient.connect("mongodb://MongoLab-r:MBC1Zu.wH4o6g02MiEDSxOml1YfGewuVHMG1Ofe9Exc-@ds050077.mongolab.com:50077/MongoLab-r", function(err, db) {
+    MongoClient.connect(connectionString, function(err, db) {
       if(!err) {
-      console.log("We are connected through azure");
+        console.log("We are connected to a mongodb server");
+        console.log("Creating document {username: user, password: password} in gmajor.users ");
+        db.collection('gmajor.users', function(err, collection){
+          if(err){
+            console.log('collection retrieval error: ', err);
+          }
+          collection.insert({username: 'user', password: 'password'}, function(err,result){
+            if(err){
+              console.log('insert into gmajor.users error: ', err);
+            }
+            console.log('insert into gmajor.users result: ', result);
+          });
+        });
+      } else {
+        console.log('mongo connection error: ', err);
       }
     });
   }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var db = require(__dirname + '/db.js');
+var db = require(__dirname + '/db/db.js');
 db.init();
 
 var port = process.env.PORT || 8080;

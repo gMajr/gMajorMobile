74446e5cdd5d16ccb89fc66a041d463813616571
William Villanueva <wvillanu89@gmail.com>
2014-07-15 11:02:34 -0700

    Revert "Deleted and cleaned up file structure"
    This reverts commit d72fb4bef42499656ab031d3edb1d033a8d430a2.

---
+++ audiolib/src/buildGrid.js
@@ -0,0 +1,15 @@
+
+// This function can be built for scaleing.
+// I also feel like this function wll be reference in the main.js file when defining a new grid.
+// the function below should call this.rows, this.cols
+// test
+var grid = function(rows, cols){
+  var result = [];
+  for ( var i = 0; i < rows; i++ ){
+    result.push([]);
+    for ( var j = 0; j < cols; j++ ){
+      result[i][j] = 0;
+    }
+  }
+  return result;
+};

d3b2cc9b2ff6964ac50e7e848a00c18a474f468f
Will LaBranche <wlabranche@gmail.com>
2014-07-16 16:20:08 -0700

    router accepts post requests to '/api/conversations/'

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var db = require(__dirname + '/db/db.js');
 var port = process.env.PORT || 8080;
@@ -9,19 +10,31 @@ server.listen(port);
 db.init();
 
 // API Router
+// handles requests starting with '/api' 
+
+server.use('/api', router);
+
+router.use(bodyParser.json());
+
 router.get('/', function(req, res){
   console.log('get /api accessed');
 });
-server.use('/api', router);
 router.route('/users')
 
   // create a bear (accessed at POST http://localhost:8080/api/users)
   .post(function(req, res) {
-    
+    // console.log(req.body);
     var username = req.body.name;  // set the users name (comes from the request)
 
     
     
+  });
+
+router.route('/conversations/:conversationId')
+  .post(function(req, res){
+    var message = req.body;
+    console.log(message);
+
   });
 // Restrictions: usernames must be unique
 // API

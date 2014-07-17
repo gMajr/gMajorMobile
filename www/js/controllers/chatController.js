d91617334d45593475c39cb6f3b8ecdfc10e5ec8
Bob Owen <bob@bobowen.org>
2014-07-16 18:11:59 -0700

    Controller keeps track of play state and button state in a chat.

--- www/js/controllers/chatController.js
+++ www/js/controllers/chatController.js
@@ -28,25 +28,91 @@ angular.module('gmajor.chatController', [])
   var wow = JSON.stringify(soundBoard.exportGrids());
   var wowza = new SoundBoard(wow);
 
+  var currentlyPlaying = undefined;
 
-  $scope.chatStream[0] = {
-    username: 'Will',
-    comment: 'This is a paragraph of text that the message send created when they created the musical grid and added it to the conversation. Bacon ipsum dolor sit amet chuck pancetta pastrami beef ribs, chicken short loin corned beef spare ribs swine hamburger. Swine frankfurter shankle sirloin flank porchetta.',
-    musicGrid: soundBoard.Grids[0],
-    playThis: function(){
-      this.musicGrid.playInterval();
+  var stopAllPlaying = function() {
+    if(currentlyPlaying !== undefined){
+      if(currentlyPlaying !== 'all') {
+        $scope.chatStream[currentlyPlaying].stopThis();
+      } else {
+        soundBoard.stopSounds();
+        $scope.playAllButtonState.style = 'button-balanced';
+        $scope.playAllButtonState.icon = 'ion-play';
+        $scope.playAllButtonState.text = 'Play All';
+      }
     }
   }
-  $scope.chatStream[1] = {
-    username: 'Bob',
-    comment: 'This is some other text. Hi mom!',
-    musicGrid: soundBoard.Grids[1],
-    playThis: function(){
-      this.musicGrid.playInterval();
-    }
+
+  $scope.playAllButtonState = {
+    style: 'button-balanced',
+    icon: 'ion-play',
+    text: 'Play All'
   }
 
   $scope.playAll = function() {
-    soundBoard.playInterval(function(){});
+    if(currentlyPlaying !== 'all') {
+      stopAllPlaying();
+      currentlyPlaying = 'all';
+      soundBoard.playInterval(function(){});
+      // Update the button since we're now playing
+      $scope.playAllButtonState.style = 'button-assertive';
+      $scope.playAllButtonState.icon = 'ion-stop';
+      $scope.playAllButtonState.text = 'Stop';
+    } else {
+      stopAllPlaying();
+      currentlyPlaying = undefined;
+    }
   }
+
+  var playThis = function(){
+    stopAllPlaying();
+    currentlyPlaying = this.id;
+    this.musicGrid.playInterval();
+    this.buttonState.style = 'button-assertive';
+    this.buttonState.icon = 'ion-stop';
+  }
+
+  var stopThis = function(){
+    currentlyPlaying = undefined;
+    this.musicGrid.stopSounds();
+    this.buttonState.style = 'button-balanced';
+    this.buttonState.icon = 'ion-play';
+  }
+
+
+  $scope.chatStream[0] = {
+    id: 0,
+    username: 'Will',
+    comment: 'This is a paragraph of text that the message send created when they created the musical grid and added it to the conversation. Bacon ipsum dolor sit amet chuck pancetta pastrami beef ribs, chicken short loin corned beef spare ribs swine hamburger. Swine frankfurter shankle sirloin flank porchetta.',
+    musicGrid: soundBoard.Grids[0],
+    togglePlay: function(){
+      if(currentlyPlaying === this.id) {
+        this.stopThis();
+      } else {
+        this.playThis();
+      }
+    },
+    playThis: playThis,
+    stopThis: stopThis,
+    buttonState: { style: 'button-balanced',
+                   icon: 'ion-play'}
+  }
+  $scope.chatStream[1] = {
+    id: 1,
+    username: 'Bob',
+    comment: 'This is some other text. Hi mom!',
+    musicGrid: soundBoard.Grids[1],
+    togglePlay: function(){
+      if(currentlyPlaying === this.id) {
+        this.stopThis();
+      } else {
+        this.playThis();
+      }
+    },
+    playThis: playThis,
+    stopThis: stopThis,
+    buttonState: { style: 'button-balanced',
+                   icon: 'ion-play'}
+  }
+
 });

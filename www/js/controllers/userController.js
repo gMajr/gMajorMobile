87883da2b49a2712d9fb7c3e74fce5eb4c67ba7f
Will LaBranche <wlabranche@gmail.com>
2014-07-18 12:16:24 -0700

    implemented sessions, access to username in scope on all pages after login

--- www/js/controllers/userController.js
+++ www/js/controllers/userController.js
@@ -6,15 +6,20 @@ angular.module('gmajor.userController', [])
         path: '/me',
         params: {fields: 'id, name'},
         success: function(user) {
-            $scope.user = user;
-            console.log($scope.user);
-            // $scope.$apply(function() {
-            //     $scope.user = user;
-            // });
+            // stores username in scope for client access
+            // stores id in token for fbdata access (I hope this isn't a security issue)
+            $scope.user = user.name;
+            // feels janky
+            window.sessionStorage['fbid'] = user.id;
         },
         error: function(error) {
             console.log(error);
-            alert('Facebook error: ' + error.error_description);
+            console.log('Facebook error: ' + error.error_description);
         }
     });
+
+  $scope.logout = function(){
+    openFb.logout();
+  };
+
 });

c2afe2de0d93c39c10b1a2be2ce7d02324f3e53e
Will LaBranche <wlabranche@gmail.com>
2014-07-20 12:23:30 -0700

    naive menu switching, part of [dev e47d6d1]]

--- www/js/services/menuService.js
+++ www/js/services/menuService.js
@@ -7,14 +7,22 @@ angular.module('gmajor.menuService', [])
 
   var menuItems = [
     { text: 'Chats', iconClass: 'icon ion-map', link: 'chats'},
-    { text: 'Chat', iconClass: 'icon ion-map', link: 'chat'}
   ];
 
-
+  // this is a bad implementation of dynamically changing login/logout buttons,
+  // it should be fixed, right now the effects only occur on reload, which feels dumb.
   return {
     all: function() {
-      return menuItems;
+      var temp = menuItems;
+      temp.push({ text: 'Login', iconClass: 'icon ion-map', link: 'login'  });
+      return temp;
+    },
+    other: function(){
+      var temp = menuItems;
+      temp.push({ text: 'Log out', iconClass: 'icon ion-map', link: 'logout' });
+      return temp;
     }
   }
+
 });
 

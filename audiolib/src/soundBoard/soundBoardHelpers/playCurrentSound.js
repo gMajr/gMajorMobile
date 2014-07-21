7726efd79876772b6a35f492983a0f1d2a726e70
William Villanueva <wvillanu89@gmail.com>
2014-07-20 22:12:41 -0700

    Commenting code

--- audiolib/src/soundBoard/soundBoardHelpers/playCurrentSound.js
+++ audiolib/src/soundBoard/soundBoardHelpers/playCurrentSound.js
@@ -1,15 +1,31 @@
+//This function plays a sound from the last item in the grid.  The last item in the grid will
+//always be the grid that the user is playing wth.
+
 SoundBoard.prototype.playSounds = function( frequencyOrSound, volume, start){
 
- var currentBoard = this.Grids[this.Grids.length -1];
- var duration = 60/currentBoard.BPM/2;
- var osc = context.createBufferSource();
- var gainNode = context.createGain();
- gainNode.gain.value = volume;
- var buffer = currentBoard.soundHash[frequencyOrSound];
- osc.buffer = buffer;
- osc.loop = false;
- osc.connect(gainNode);
- gainNode.connect(context.destination);
- osc.start(start);
+  var currentBoard = this.Grids[this.Grids.length -1];
+  //The currentBoard is the board that the user will currently be working with.  It will allways
+  //be the last board in the Grids array
+  var duration = 60/currentBoard.BPM/2;
+  //This is the duration of the sound
+  var osc = context.createBufferSource();
+  //The sound data, PCM data is stored in a buffer.
+  var gainNode = context.createGain();
+  //The gain node lets you alter the volume of the sound
+  gainNode.gain.value = volume;
+  //here, we store look to our soundhash table to get the sound data and play it.
+  var buffer = currentBoard.soundHash[frequencyOrSound];
+  osc.buffer = buffer;
+  //if this is not set to false, then the sound will continue to loop and play.
+  osc.loop = false;
+  osc.connect(gainNode);
+  //Here, you connect the sound to the gain node.  The gain node acts as a processing filter and 
+  //can make the sound louder or quieter.  It is simply volume.
+  gainNode.connect(context.destination);
+  //Above, you connect the gain node to the speaker.  context.destination represents the computer speakers.
+  //the web audio API has a precise timer that will play a sound whenever you'd schedule it for. 
+  //context.currentTime gives you the current time.  If you give a time that is in the past, then it will 
+  //play immediately.
+  osc.start(start);
 
 };
--- audiolib/src/soundBoard/soundBoardHelpers/toggle.js
+++ audiolib/src/soundBoard/soundBoardHelpers/toggle.js
@@ -1,7 +1,15 @@
+
+
 SoundBoard.prototype.toggle = function(col, row){
 
 	var amountOfGrids = this.Grids.length -1;
+	//There is a special case if we're toggling the drum sounds.  Instead of frequency numbers, we play 
+	//sound names.  ie. 'hh', 'kick', 'synrider'.  Wherease the synthesized sounds are called 
+	//by numbers. ie. 373.2, 478 Hz, etc.
+
 	currentBoard = this.Grids[amountOfGrids];
+	//current grid is always the last grid in the Grids array.
+	//The keys, or specific frequencies are stored in a keys array
 	if ( currentBoard.instrumentName === 'drums' ){
 		if( currentBoard.keys[row] in currentBoard.noteScheduler[col]){
 			delete currentBoard.noteScheduler[col][currentBoard.keys[row]];
@@ -9,6 +17,9 @@ SoundBoard.prototype.toggle = function(col, row){
 			currentBoard.noteScheduler[col][currentBoard.keys[row]] = currentBoard.keys[row];
 		}
 	}else{		
+		//we had to put a number without a decimal as a key.  A decimal in the key will not work with 
+		//mongodb.  In this case we give an integer as the key, and the full decimal frequency as the value.
+		
 		if (parseInt(currentBoard.keys[row]) in currentBoard.noteScheduler[col]){
 		 delete currentBoard.noteScheduler[col][parseInt(currentBoard.keys[row])];
 		}else{

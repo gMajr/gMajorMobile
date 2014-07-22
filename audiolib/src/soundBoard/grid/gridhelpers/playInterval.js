f70a0c98ffa4d985b7ce405f1435640b7b2317f1
Will LaBranche <wlabranche@gmail.com>
2014-07-21 18:26:48 -0700

    cleaned up for readability

--- audiolib/src/soundBoard/grid/gridhelpers/playInterval.js
+++ audiolib/src/soundBoard/grid/gridhelpers/playInterval.js
@@ -2,33 +2,31 @@
 
 Grid.prototype.playInterval = function(){
  var startTime = context.currentTime;
- var halfwayPointBetweenNotes = 60/this.BPM/2/2;
+ var halfwayPointBetweenNotes = 60 / this.BPM / 2 / 2;
  var MIDI = this.noteScheduler;
 
 
- var continuedLoop = function( MIDI, startTime, k){
+ var continuedLoop = function( MIDI, startTime, k ){
    var currentTime = context.currentTime;
-   var currentCol = Math.ceil((currentTime - startTime)/(60/this.BPM/2)%8)-1;
-   var numberOfCycles = Math.floor((currentTime - startTime)/(60/this.BPM/2*8))
-   var scheduledTime = ((numberOfCycles * ( 60/this.BPM/2 * 8 )) + startTime + ((currentCol + 1) * 60/this.BPM/2))
-   for (var note in this.noteScheduler[currentCol]){
+   var currentCol = Math.ceil( ( currentTime - startTime ) / ( 60 / this.BPM / 2 ) % 8 ) - 1;
+   var numberOfCycles = Math.floor( ( currentTime - startTime ) / ( 60 / this.BPM / 2 * 8 ) );
+   var scheduledTime = ( ( numberOfCycles * ( 60 / this.BPM / 2 * 8 ) ) + startTime + ( ( currentCol + 1 ) * 60 / this.BPM / 2 ) );
+   for ( var note in this.noteScheduler[currentCol] ){
      if ( this.noteScheduler[currentCol][note] ){
-       this.playSounds( this.noteScheduler[currentCol][note], 1, scheduledTime )
+       this.playSounds( this.noteScheduler[currentCol][note], 1, scheduledTime );
      }
    }
-
-   loop.call( this, MIDI, startTime, halfwayPointBetweenNotes, continuedLoop, (scheduledTime - context.currentTime + halfwayPointBetweenNotes), k);
+   loop.call( this, MIDI, startTime, halfwayPointBetweenNotes, continuedLoop, (scheduledTime - context.currentTime + halfwayPointBetweenNotes), k );
  };
 
- var loop = function( MIDI, startTime, halfwayPointBetweenNotes, continuedLoop, firstTime , k){
-
+ var loop = function( MIDI, startTime, halfwayPointBetweenNotes, continuedLoop, firstTime , k ){
    k = k || 0;
    k++;
    firstTime = firstTime || 0;
-   this.interval = setTimeout( continuedLoop.bind(this, MIDI, startTime, k), firstTime * 1000);
+   this.interval = setTimeout( continuedLoop.bind( this, MIDI, startTime, k ), firstTime * 1000 );
+ };
+ if ( !context.currentTime ){
+   this.playSounds( this.keys[0], 0, 0 );
  }
- if (!context.currentTime){
-   this.playSounds(this.keys[0], 0, 0);
- }
- loop.call( this, MIDI, startTime, halfwayPointBetweenNotes, continuedLoop);
+ loop.call( this, MIDI, startTime, halfwayPointBetweenNotes, continuedLoop );
 };

abf0a8c1407950d6db1c725222856ea503f7ac1f
Christopher Gray <areagray@gmail.com>
2014-07-24 18:07:59 -0700

    Restyle Profile Image Add Game Link on Profile Restyle Chat Stream List

--- www/js/services/userService.js
+++ www/js/services/userService.js
@@ -8,7 +8,8 @@ angular.module('gmajor.userService', [])
       params: {fields: 'id, name, email'},
       success: function(user) {
         // stores user/id in scope for client access
-        window.sessionStorage['photoUrl'] = 'https://graph.facebook.com/' + user.id+ '/picture?type=square'
+        window.sessionStorage['photoUrl'] = 'https://graph.facebook.com/' + user.id+ '/picture?type=large';
+        console.log('user id', user.id);
         window.sessionStorage['fbid'] = user.id;
         window.sessionStorage['name'] = user.name;
         window.sessionStorage['email'] = user.email;

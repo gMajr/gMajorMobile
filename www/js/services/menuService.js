d0955ecd89f73b45ff8ae4d0a29b52e3357af15c
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-25 14:16:57 -0700

    Touch up style on chats view

--- www/js/services/menuService.js
+++ www/js/services/menuService.js
@@ -6,6 +6,7 @@ angular.module('gmajor.menuService', [])
 .factory('MenuService', function() {
 
   var menuItems = [
+    { text: 'New Sound', iconClass: 'icon ion-play', link: 'grid'},
     { text: 'Chats', iconClass: 'icon ion-music-note', link: 'chats'},
     { text: 'Game', iconClass: 'icon ion-game-controller-b', link: 'game'},
     { text: 'Profile', iconClass: 'icon ion-person', link: 'profile'},
